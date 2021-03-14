5eee262528bcd09b32f8a864df82ee29d8b8d94a
StanimalTheMan <sjc591@nyu.edu>
2021-03-14 19:11:34 -0400

    temp fix for github pages deploy

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom"; // attempt at seo issue w/ github pages
+import { HashRouter } from "react-router-dom";
 
 import Pokedex from "./containers/Pokedex/Pokedex";
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter basename="/pokedex">
+      // <BrowserRouter basename="/pokedex">
+      //   <Pokedex />
+      // </BrowserRouter>
+      <HashRouter basename="/pokedex">
         <Pokedex />
-      </BrowserRouter>
+      </HashRouter>
     );
   }
 }
